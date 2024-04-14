6e49f01360fd5eb6f8fd02371b120c9d7b4af7ba
raselualam <raselualam@gmail.com>
2024-04-14 12:26:45 -0400

    class

---
+++ src/test/java/upskill/ebay/pageAction/EbayHomePageActions.java
@@ -0,0 +1,21 @@
+package upskill.ebay.pageAction;
+
+import org.openqa.selenium.support.PageFactory;
+
+import upskill.ebay.pageElements.EbayHomePageLocators;
+import upskill.utilities.SetupDrivers;
+
+public class EbayHomePageActions {
+	
+	EbayHomePageLocators EbayHomePageLocatorsObj;
+	
+	public EbayHomePageActions(){
+		EbayHomePageLocatorsObj = new EbayHomePageLocators();
+		PageFactory.initElements(SetupDrivers.driver, EbayHomePageLocatorsObj);
+	}
+	
+	public void searchShoes(){
+		EbayHomePageLocatorsObj.txtbxSearch.sendKeys("Shoes");
+		EbayHomePageLocatorsObj.btnSearch.click();
+	}
+}
---
+++ src/test/java/upskill/ebay/stepDef/EbaySearchResultSteps.java
@@ -0,0 +1,25 @@
+package upskill.ebay.stepDef;
+
+import cucumber.api.java.en.Then;
+import upskill.ebay.pageAction.EbaySearchResultActions;
+
+public class EbaySearchResultSteps {
+	
+	EbaySearchResultActions EbaySearchResultActionsObj = new EbaySearchResultActions();
+	
+	@Then("^Item list should have only shoes related products$")
+	public void item_list_should_have_only_shoes_related_products() throws Throwable {
+		EbaySearchResultActionsObj.verifyShoesItems();
+	}
+	
+	@Then("^Item list should have only Shirts related products$")
+	public void item_list_should_have_only_Shirts_related_products() throws Throwable {
+	    
+	}
+	
+	@Then("^Item list should have only Pants related products$")
+	public void item_list_should_have_only_Pants_related_products() throws Throwable {
+	    
+	}
+
+}
